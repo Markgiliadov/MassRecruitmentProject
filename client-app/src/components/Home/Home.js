ba7e2ab9bbdca5738b0c7c88221f10ca41730f88
Mark giliadov <markgiliadov@gmail.com>
2022-08-08 13:54:11 +0300

    test1

--- client-app/src/components/Home/Home.js
+++ client-app/src/components/Home/Home.js
@@ -180,18 +180,18 @@ const Home = () => {
   }
   return (
     <>
-      {/* <h1 className={classes.h1}>In-progress projects</h1> */}
+      <h1 className={classes.h1}>In-progress projects</h1>
       <div style={{ overflowX: "scroll" }} className={classes.projectList_wrap}>
         {inProgressProjects}
       </div>
-      {/* <h1 className={classes.h1}>Incomplete projects</h1> */}
+      <h1 className={classes.h1}>Incomplete projects</h1>
       {/* {incompleteProjects ? ( */}
       <div style={{ overflowX: "scroll" }} className={classes.projectList_wrap}>
         {incompleteProjects}
       </div>
       {/* ) : null} */}
 
-      {/* <h1 className={classes.h1}>Completed projects</h1> */}
+      <h1 className={classes.h1}>Completed projects</h1>
       <div style={{ overflowX: "scroll" }} className={classes.projectList_wrap}>
         {completeProjects}
       </div>
